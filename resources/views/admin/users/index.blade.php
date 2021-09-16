dabe3cf77b91052dfc29203aea3801d60c4c932d
Anowar <1hafizajr@gmail.com>
2021-09-16 19:59:01 +0600

    add user table

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -11,27 +11,17 @@
                 <th scope="col">Action</th>
               </tr>
             </thead>
-            
+            @foreach ($users as $user)
             <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-              </tr>
-            </tbody>
-          </table>
+                <tr>
+                  <th scope="row">{{$user->id}}</th>
+                  <td>{{$user->name}}</td>
+                  <td>{{$user->email}}</td>
+                  </tr>
+                <tr>
+                </tbody>
+            @endforeach
+            </table>
     </div>
 @endsection
  
