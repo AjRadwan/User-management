0ed19b5fac54258e2522e7456742b5e28c492556
Anowar <1hafizajr@gmail.com>
2021-09-16 19:08:59 +0600

    create admin folder

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -14,7 +14,7 @@ class UserController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function index() {
-       return view('admin.user.index', ['users' => User::all()]);
+       return view('admin.users.index', ['users' => User::all()]);
     }
 
     /**
