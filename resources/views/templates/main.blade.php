ff23048db4f493a1da37ace1ff6ba528b2c8049e
Anowar <1hafizajr@gmail.com>
2021-09-16 12:37:44 +0600

    add user role

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -31,20 +31,26 @@
     </li>
  </ul>
 <div class="d-flex">
-       @if (Route::has('login'))
-            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-                @auth
-                    <a href="{{ url('/home') }}" class="">Home</a>
-                @else
-                    <a href="{{ route('login') }}" class="">Log in</a>
+@if (Route::has('login'))
+    <div >
+        @auth
+            <a href="{{ url('/home') }}" >Home</a>
+            <a href="{{ url('/logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">logout</a>
 
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}" class="">Register</a>
-                    @endif
-                @endauth
-            </div>
-        @endif 
+            <form  id="logout-form" action="{{route('logout')}}" method="POST" style="display: none">
+                @csrf
+
+            </form>
+        @else
+            <a href="{{ route('login') }}" >Log in</a>
+
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}" >Register</a>
+            @endif
+        @endauth
     </div>
+@endif 
+</div>
     </div>
     </div>
 
